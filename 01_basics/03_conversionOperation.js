c32ce4cdaaa620f3a429c6e9dde48ca80478c171
atul675 <atuldubey7307@gmail.com>
2026-06-18 10:36:29 +0000

    updated operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -14,9 +14,32 @@ let valueInNumber = Number(score)
 
 let isLoggedIn = "Atul"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
 // "Atul" => true
+
+// OPERATIONS //
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = "atul"
+
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
+
+
+

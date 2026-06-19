7267d91f5c7dbf7d1964ea6d36ad55a10e7417aa
atul675 <atuldubey7307@gmail.com>
2026-06-19 06:17:44 +0000

    comparison_operator

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,9 @@
+console.log(3>2);
+console.log(2<3);
+console.log(2==2);
+console.log(undefined==0);
+console.log(undefined>=0);
+console.log("03">2);
+console.log(null<0);
+console.log(null<0);
+console.log(nul>=0);

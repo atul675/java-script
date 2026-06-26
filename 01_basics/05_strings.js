7da3754eaeed0c2ca98aaa10283ffdcbc254c28d
atul675 <atuldubey7307@gmail.com>
2026-06-26 08:30:09 +0000

    Add 05_strings.js

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,32 @@
+const name = "atul"
+const repocount = 20
+
+//console.log(name + repocount + "value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+
+const gamename = new String('atul-du')
+
+console.log(gamename [0]);
+console.log(gamename.__proto__);
+
+
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(3));
+console.log(gamename.indexOf('t'));
+
+const newString = gamename.substring(0 , 4)
+console.log(newString);
+
+const anotherString = gamename.slice(-4, 4)
+console.log(anotherString);
+
+const newStringOne = "   atul   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+
+
+

e75db8a10c1c7b3bdc65125e43e09e0e8f47580b
atul675 <atuldubey7307@gmail.com>
2026-07-01 09:43:36 +0000

    IIFE in js

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,12 @@
+// Immediately Invoked Function Expressionss (IIEFE)
+
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+})('atul')              
+

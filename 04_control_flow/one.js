853c98114e8c3bbb8b1484b8ce10e2c248cee352
atul675 <atuldubey7307@gmail.com>
2026-07-02 06:11:58 +0000

    checking statements

---
+++ 04_control_flow/one.js
@@ -0,0 +1,45 @@
+// if
+const isUserloggedIn = true
+const temprature = 40
+
+// if (temprature == 55){
+//     console.log("less than 50");
+// } else{
+//     console.log("temprature is greater");
+// }
+
+// <,>,<=,>=,==,!=,===,!==
+
+// const score = 200
+// if (score>100){
+//     let power = "fly"
+//     console.log(`user power: ${power}`);
+// }
+// console.log(`user power : ${power}`);
+
+const balance = 2000
+// if (balance>1000) console.log("pass");console.log("pass2");
+
+// if (balance<200){
+//     console.log("less than 200");
+// }else if (balance<300){
+//     console.log("less than 300");
+// }else if (balance<500){
+//     console.log("less than 500")
+// }else{
+//     console.log("less than 3000");
+// }
+
+const userLoggedIn = true
+const debitcard = true
+const loggedInfromgoogle = false
+const loggedInfromEmail = true
+
+if (userLoggedIn && debitcard){
+    console.log("Allow to buy")
+}
+if ( loggedInfromgoogle  || loggedInfromEmail){
+    console.log("Allow user");
+
+}
+
